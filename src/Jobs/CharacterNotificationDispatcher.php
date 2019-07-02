0d9af9bbf84ca3ca69e4555663ce98e1f2a0a740
Justin Mercer <justin.mercer9013@gmail.com>
2019-07-02 12:09:18 -0600

    ----

--- src/Jobs/CharacterNotificationDispatcher.php
+++ src/Jobs/CharacterNotificationDispatcher.php
@@ -72,7 +72,7 @@ class CharacterNotificationDispatcher extends SeatNotificationsJobBase
     {
         Cache::lock('notification_id_'.$this->notification_id)->get(function () use ($abstractClass) {
             if(Cache::get('notification_id_'.$this->notification_id)) {
-                logger()->debug('A character notification job is already running for (cache) ' . $this->notification_id);
+                logger()->debug('A character notification job is already running for (cache) ' . $this->notification_id . ' : ' . Cache::get('notification_id_'.$this->notification_id));
                 return;
             }
             Cache::put('notification_id_'.$this->notification_id, true, 7201);
